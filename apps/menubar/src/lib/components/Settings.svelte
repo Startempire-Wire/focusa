97f579c8247db19f5ec5a5272c35191a013a5d89
Verious Smith <verious.smith@philoveracity.com>
2026-07-10 02:08:32 -0700

    chore: stamp menubar 0.9.78-dev

--- apps/menubar/src/lib/components/Settings.svelte
+++ apps/menubar/src/lib/components/Settings.svelte
@@ -245,7 +245,7 @@
   </section>
 
   <section class="section about">
-    <span>Focusa v0.9.77-dev</span><span>·</span><span>Cognitive Governance</span>
+    <span>Focusa v0.9.78-dev</span><span>·</span><span>Cognitive Governance</span>
   </section>
 </div>
 
