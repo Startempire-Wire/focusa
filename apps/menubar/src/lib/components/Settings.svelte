e3ff67d8b19251d8ffffc4357631aab23a680dd9
Verious Smith <verious.smith@philoveracity.com>
2026-08-11 10:22:48 -0700

    chore: stamp release surfaces 0.9.148

--- apps/menubar/src/lib/components/Settings.svelte
+++ apps/menubar/src/lib/components/Settings.svelte
@@ -287,7 +287,7 @@
   </section>
 
   <section class="section about">
-    <span>Focusa v0.9.147</span><span>·</span><span>Cognitive Governance</span>
+    <span>Focusa v0.9.148</span><span>·</span><span>Cognitive Governance</span>
   </section>
 </div>
 
